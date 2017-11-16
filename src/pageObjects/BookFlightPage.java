3150ecc854bc050588791f91dc5138add6f73d37
guerrerov270 <jguerrero089@gmail.com>
2017-11-16 13:46:27 -0500

    Reserva para multiples pasajeros

--- src/pageObjects/BookFlightPage.java
+++ src/pageObjects/BookFlightPage.java
@@ -28,6 +28,27 @@ public class BookFlightPage extends PageBase {
 	@FindBy(how = How.NAME, using = "pass.0.meal")
 	private WebElement comboMeal;
 
+	@FindBy(how = How.NAME, using = "passFirst1")
+	private WebElement inputFirstName2;
+	@FindBy(how = How.NAME, using = "passLast1")
+	private WebElement inputLastName2;
+	@FindBy(how = How.NAME, using = "pass.1.meal")
+	private WebElement comboMeal2;
+
+	@FindBy(how = How.NAME, using = "passFirst2")
+	private WebElement inputFirstName3;
+	@FindBy(how = How.NAME, using = "passLast2")
+	private WebElement inputLastName3;
+	@FindBy(how = How.NAME, using = "pass.2.meal")
+	private WebElement comboMeal3;
+
+	@FindBy(how = How.NAME, using = "passFirst3")
+	private WebElement inputFirstName4;
+	@FindBy(how = How.NAME, using = "passLast3")
+	private WebElement inputLastName4;
+	@FindBy(how = How.NAME, using = "pass.3.meal")
+	private WebElement comboMeal4;
+
 	// Área Credit Card
 	@FindBy(how = How.NAME, using = "creditCard")
 	private WebElement comboCardType;
@@ -250,4 +271,121 @@ public class BookFlightPage extends PageBase {
 
 	}// Fin método seleccionBookAFlight
 
-}
+	public void seleccionBookAFlightMultiplesPasajeros(int pasajeros,
+			String firstName0, String lastName0, String meal0,
+			String firstName1, String lastName1, String meal1,
+			String firstName2, String lastName2, String meal2,
+			String firstName3, String lastName3, String meal3, String cardType,
+			String cardNumber, String expCMonth, String expCYear,
+			String cardFirstName, String cardMiddleNane, String cardLastName,
+			boolean ticketless, String address1Bill, String address2Bill,
+			String cityBill, String stateBill, String postalCodeBill,
+			String countryBill, boolean sameAsBillingAddress,
+			String address1Del, String address2Del, String cityDel,
+			String stateDel, String postalCodeDel, String countryDelNoAlert,
+			String countryDelAlert) {
+
+		switch (pasajeros) {
+		case 1:
+			sendText(inputFirstName, firstName0);
+			sendText(inputLastName, lastName0);
+			selectDropdownVisibleText(comboMeal, meal0);
+			break;
+		case 2:
+			sendText(inputFirstName, firstName0);
+			sendText(inputLastName, lastName0);
+			selectDropdownVisibleText(comboMeal, meal0);
+
+			sendText(inputFirstName2, firstName1);
+			sendText(inputLastName2, lastName1);
+			selectDropdownVisibleText(comboMeal2, meal1);
+			break;
+		case 3:
+			sendText(inputFirstName, firstName0);
+			sendText(inputLastName, lastName0);
+			selectDropdownVisibleText(comboMeal, meal0);
+
+			sendText(inputFirstName2, firstName1);
+			sendText(inputLastName2, lastName1);
+			selectDropdownVisibleText(comboMeal2, meal1);
+
+			sendText(inputFirstName3, firstName2);
+			sendText(inputLastName3, lastName2);
+			selectDropdownVisibleText(comboMeal3, meal2);
+
+			break;
+		case 4:
+			sendText(inputFirstName, firstName0);
+			sendText(inputLastName, lastName0);
+			selectDropdownVisibleText(comboMeal, meal0);
+
+			sendText(inputFirstName2, firstName1);
+			sendText(inputLastName2, lastName1);
+			selectDropdownVisibleText(comboMeal2, meal1);
+
+			sendText(inputFirstName3, firstName2);
+			sendText(inputLastName3, lastName2);
+			selectDropdownVisibleText(comboMeal3, meal2);
+
+			sendText(inputFirstName4, firstName3);
+			sendText(inputLastName4, lastName3);
+			selectDropdownVisibleText(comboMeal4, meal3);
+
+			break;
+		}
+
+		selectDropdownVisibleText(comboCardType, cardType);
+		sendText(inputNumber, cardNumber);
+		selectDropdownVisibleText(comboExpirationMonth, expCMonth);
+		selectDropdownVisibleText(comboExpirationYear, expCYear);
+		sendText(inputFirstNameCard, cardFirstName);
+		sendText(inputMiddleNameCard, cardMiddleNane);
+		sendText(inputLastNameCard, cardLastName);
+
+		if (ticketless) {
+			// Selecciono el check
+			checkTicketLess.click();
+		}
+
+		inputBillAddress1.clear();
+		sendText(inputBillAddress1, address1Bill);
+		sendText(inputBillAddress2, address2Bill);
+		inputBillCity.clear();
+		sendText(inputBillCity, cityBill);
+		inputBillState.clear();
+		sendText(inputBillState, stateBill);
+		inputBillPostal.clear();
+		sendText(inputBillPostal, postalCodeBill);
+		selectDropdownVisibleText(comboBillCountry, countryBill);
+
+		if (sameAsBillingAddress) {
+			// Selecciono el check
+			checkBillingAddress.click();
+		}
+
+		inputDelAddress1.clear();
+		sendText(inputDelAddress1, address1Del);
+		sendText(inputDelAddress2, address2Del);
+		inputDelCity.clear();
+		sendText(inputDelCity, cityDel);
+		inputDelState.clear();
+		sendText(inputDelState, stateDel);
+		inputDelPostal.clear();
+		sendText(inputDelPostal, postalCodeDel);
+
+		// Si el valor que viene por parámetro es UNITED STATES No saldrá
+		// ninguna alerta, si es otro distinto va a salir una alerta que hay que
+		// aceptar para poder continuar con la reserva del vuelo
+
+		if (countryDelNoAlert != countryDelAlert) {
+			selectDropdownVisibleText(comboDelCountry, countryDelAlert);
+			driver.switchTo().alert().accept();
+		} else {
+			selectDropdownVisibleText(comboDelCountry, countryDelNoAlert);
+		}
+
+		clickButtonLink(buttonBuyFlights);
+
+	}// Fin método seleccionBookAFlightMultiplesPasajeros
+
+}// Fin clase BookFlightPage
---
+++ src/test/reservarvuelo/cp007ReservarVueloMultiplesPasajeros.java
@@ -0,0 +1,217 @@
+package test.reservarvuelo;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import base.TestBase;
+import pageObjects.BookFlightPage;
+import pageObjects.FindFlightPage;
+import pageObjects.FlightConfirmationPage;
+import pageObjects.PagesFacade;
+import pageObjects.SelectFlightPage;
+import utils.ExcelUtils;
+import utils.ExcelUtils.ExcelType;
+
+public class cp007ReservarVueloMultiplesPasajeros extends TestBase {
+
+	PagesFacade facade;
+	FindFlightPage findFlight;
+	SelectFlightPage selectFlight;
+	BookFlightPage bookFlight;
+	FlightConfirmationPage confirmationFlight;
+	static ExcelUtils excelDatosPaginas;
+	// Valores para seleccionar las opciones en la página Find Flight
+	private String tripType = "";
+	private String passengers = "";
+	private String departing = "";
+	private String monthIni = "";
+	private String dayIni = "";
+	private String arrivingIn = "";
+	private String monthEnd = "";
+	private String dayEnd = "";
+	private String serviceClass = "";
+	private String airline = "";
+
+	// Valores para seleccionar las opciones en la página Select Flight
+	private String departingSelect = "";
+	private String returningSelect = "";
+
+	// Valores para seleccionar las opciones en la página Book a flight
+	private String firstName0 = "";
+	private String lastName0 = "";
+	private String meal0 = "";
+
+	private String firstName1 = "";
+	private String lastName1 = "";
+	private String meal1 = "";
+
+	private String firstName2 = "";
+	private String lastName2 = "";
+	private String meal2 = "";
+
+	private String firstName3 = "";
+	private String lastName3 = "";
+	private String meal3 = "";
+
+	private String cardType = "";
+	private String cardNumber = "";
+	private String expCMonth = "";
+	private String expCYear = "";
+	private String cardFirstName = "";
+	private String cardMiddleNane = "";
+	private String cardLastName = "";
+	private String auxticketless = "";
+	private boolean ticketless = false;
+	private String address1Bill = "";
+	private String address2Bill = "";
+	private String cityBill = "";
+	private String stateBill = "";
+	private String postalCodeBill = "";
+	private String countryBill = "";
+	private String auxsameAsBillingAddress = "";
+	boolean sameAsBillingAddress = false;
+	private String address1Del = "";
+	private String address2Del = "";
+	private String cityDel = "";
+	private String stateDel = "";
+	private String postalCodeDel = "";
+	private String countryDelNoAlert = "";
+	private String countryDelAlert = "";
+
+	// Valores para seleccion en la página Confirmation Flight
+	private String eleccion = "";
+
+	@Test
+	public void login() throws FileNotFoundException, IOException {
+
+		facade = new PagesFacade();
+		facade.hacerLogin();
+	}
+
+	@Test(priority = 1, dependsOnMethods = { "login" })
+	public void diligenciarPaginasRestantes() throws Exception {
+
+		facade = new PagesFacade();
+		findFlight = facade.getFindFlight();
+		// Diligencio la página con valores desde el data pool
+		excelDatosPaginas = new ExcelUtils("DatosFindFlight.xlsx",
+				ExcelType.XLSX);
+		tripType = excelDatosPaginas.getCellData(1, 1);
+		passengers = excelDatosPaginas.getCellData(2, 4);
+		departing = excelDatosPaginas.getCellData(3, 1);
+		monthIni = excelDatosPaginas.getCellData(4, 1);
+		dayIni = excelDatosPaginas.getCellData(5, 1);
+		arrivingIn = excelDatosPaginas.getCellData(6, 1);
+		monthEnd = excelDatosPaginas.getCellData(7, 1);
+		dayEnd = excelDatosPaginas.getCellData(8, 1);
+		serviceClass = excelDatosPaginas.getCellData(9, 1);
+		airline = excelDatosPaginas.getCellData(10, 1);
+		findFlight.seleccionFindFlight(tripType, passengers, departing,
+				monthIni, dayIni, arrivingIn, monthEnd, dayEnd, serviceClass,
+				airline);
+
+		// Dependiendo del número de pasajeros se ejecutarán las pruebas
+		diligenciarSelectFlight();
+		diligenciarBookFlight(Integer.parseInt(passengers));
+		verificarConfirmationFlight();
+	}
+
+	public void diligenciarSelectFlight() throws Exception {
+
+		facade = new PagesFacade();
+		selectFlight = facade.getSelectFlight();
+		// Diligencio la página con valores desde el data pool
+		excelDatosPaginas = new ExcelUtils("DatosSelectFlight.xlsx",
+				ExcelType.XLSX);
+		departingSelect = excelDatosPaginas.getCellData(1, 1);
+		returningSelect = excelDatosPaginas.getCellData(2, 1);
+		selectFlight.seleccionSelectFlight(departingSelect, returningSelect);
+
+	}
+
+	public void diligenciarBookFlight(int pasajeros) throws Exception {
+
+		facade = new PagesFacade();
+		bookFlight = facade.getBookFlight();
+
+		// Diligencio la página con valores desde el data pool
+		excelDatosPaginas = new ExcelUtils("DatosBookAFlight.xlsx",
+				ExcelType.XLSX);
+		firstName0 = excelDatosPaginas.getCellData(1, 1);
+		lastName0 = excelDatosPaginas.getCellData(2, 1);
+		meal0 = excelDatosPaginas.getCellData(3, 1);
+
+		firstName1 = excelDatosPaginas.getCellData(1, 2);
+		lastName1 = excelDatosPaginas.getCellData(2, 2);
+		meal1 = excelDatosPaginas.getCellData(3, 2);
+
+		firstName2 = excelDatosPaginas.getCellData(1, 3);
+		lastName2 = excelDatosPaginas.getCellData(2, 3);
+		meal2 = excelDatosPaginas.getCellData(3, 3);
+
+		firstName3 = excelDatosPaginas.getCellData(1, 4);
+		lastName3 = excelDatosPaginas.getCellData(2, 4);
+		meal3 = excelDatosPaginas.getCellData(3, 4);
+
+		cardType = excelDatosPaginas.getCellData(4, 1);
+		cardNumber = excelDatosPaginas.getCellData(5, 1);
+		expCMonth = excelDatosPaginas.getCellData(6, 1);
+		expCYear = excelDatosPaginas.getCellData(7, 1);
+		cardFirstName = excelDatosPaginas.getCellData(8, 1);
+		cardMiddleNane = excelDatosPaginas.getCellData(9, 1);
+		cardLastName = excelDatosPaginas.getCellData(10, 1);
+		auxticketless = excelDatosPaginas.getCellData(11, 1);
+		if (auxticketless.equals("true")) {
+			ticketless = true;
+		} else if (auxticketless.equals("false")) {
+			ticketless = false;
+		}
+		address1Bill = excelDatosPaginas.getCellData(12, 1);
+		address2Bill = excelDatosPaginas.getCellData(13, 1);
+		cityBill = excelDatosPaginas.getCellData(14, 1);
+		stateBill = excelDatosPaginas.getCellData(15, 1);
+		postalCodeBill = excelDatosPaginas.getCellData(16, 1);
+		countryBill = excelDatosPaginas.getCellData(17, 1);
+		auxsameAsBillingAddress = excelDatosPaginas.getCellData(18, 1);
+		if (auxsameAsBillingAddress.equals("true")) {
+			sameAsBillingAddress = true;
+		} else if (auxsameAsBillingAddress.equals("false")) {
+			sameAsBillingAddress = false;
+		}
+		address1Del = excelDatosPaginas.getCellData(19, 1);
+		address2Del = excelDatosPaginas.getCellData(20, 1);
+		cityDel = excelDatosPaginas.getCellData(21, 1);
+		stateDel = excelDatosPaginas.getCellData(22, 1);
+		postalCodeDel = excelDatosPaginas.getCellData(23, 1);
+
+		// Con este valor no se abre ninguna alerta "UNITED STATES"
+		countryDelNoAlert = excelDatosPaginas.getCellData(24, 1);
+
+		// Con este valor se abre una alerta que hay que aceptar
+		countryDelAlert = excelDatosPaginas.getCellData(24, 2);
+
+		bookFlight.seleccionBookAFlightMultiplesPasajeros(pasajeros,
+				firstName0, lastName0, meal0, firstName1, lastName1, meal1,
+				firstName2, lastName2, meal2, firstName3, lastName3, meal3,
+				cardType, cardNumber, expCMonth, expCYear, cardFirstName,
+				cardMiddleNane, cardLastName, ticketless, address1Bill,
+				address2Bill, cityBill, stateBill, postalCodeBill, countryBill,
+				sameAsBillingAddress, address1Del, address2Del, cityDel,
+				stateDel, postalCodeDel, countryDelNoAlert, countryDelAlert);
+	}
+
+	public void verificarConfirmationFlight() throws Exception {
+
+		facade = new PagesFacade();
+		confirmationFlight = facade.getConfirmationFlight();
+		// Diligencio la página con valores desde el data pool
+		excelDatosPaginas = new ExcelUtils("DatosConfirmationFlight.xlsx",
+				ExcelType.XLSX);
+		eleccion = excelDatosPaginas.getCellData(1, 1);
+		confirmationFlight.seleccionarBotonFlightConfirmation(eleccion);
+
+	}
+
+}
