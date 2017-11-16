79e94f7abc6a8b33efe6c4539f134112aa7d4bd3
guerrerov270 <jguerrero089@gmail.com>
2017-11-16 18:08:48 -0500

    Links superiores

--- src/test/menu/cp001ValidarLinkSuperior.java
+++ src/test/menu/cp001ValidarLinkSuperior.java
@@ -8,38 +8,60 @@ import org.testng.annotations.Test;
 
 import base.TestBase;
 import pageObjects.MenuNavegacionPage;
+import pageObjects.PagesFacade;
+import utils.ElementoNoEncontradoException;
 import utils.ExcelUtils;
 import utils.ExcelUtils.ExcelType;
 
 public class cp001ValidarLinkSuperior extends TestBase {
+
+	PagesFacade facade;
 	MenuNavegacionPage menu;
-	String pageTitleIndex = "Welcome: Mercury Tours";
-	private String lnkSingOn="";
-	private String lnkRegister="";
-	private String lnkSupport=""; 
-	private String lnkContact=""; 
+	private String lnkSingOn = "";
+	private String lnkRegister = "";
+	private String lnkSupport = "";
+	private String lnkContact = "";
 
 	// Utilidad para el manejo de excel
 	static ExcelUtils excelLinksMenu;
 
 	@Test
-	public void validarLinkSuperior() {
+	public void validarLinkSuperior() throws FileNotFoundException, IOException {
+
+		facade = new PagesFacade();
 		// Se crea la instancia para manejar el archivo LinksMenu.xlsx
 		try {
 			excelLinksMenu = new ExcelUtils("LinksMenu.xlsx", ExcelType.XLSX);
-			menu = new MenuNavegacionPage(driver, pageTitleIndex);
-			// Validar se encuentren los links con su correspondiente texto
-			// SIGN-ON REGISTER SUPPORT CONTACT
-			lnkSingOn=excelLinksMenu.getCellData(0, 1);
-			if (!(menu.getTextElement(menu.getLnkSingOn(), lnkSingOn))){
-				Assert.fail("No se encontró el link " + lnkSingOn);
+			menu = facade.getMenuPage();
+
+			// Cargamos el texto de los links desde el excel
+			lnkSingOn = excelLinksMenu.getCellData(0, 1);
+			lnkRegister = excelLinksMenu.getCellData(0, 2);
+			lnkSupport = excelLinksMenu.getCellData(0, 3);
+			lnkContact = excelLinksMenu.getCellData(0, 4);
+
+			if (!(menu.getTextElement(menu.getLnkSingOn(), lnkSingOn))) {
+				throw new ElementoNoEncontradoException(lnkSingOn);
 			}
+			if (!(menu.getTextElement(menu.getLnkSingOn(), lnkSingOn))) {
+				throw new ElementoNoEncontradoException(lnkRegister);
+			}
+			if (!(menu.getTextElement(menu.getLnkSingOn(), lnkSupport))) {
+				throw new ElementoNoEncontradoException(lnkSupport);
+			}
+			if (!(menu.getTextElement(menu.getLnkSingOn(), lnkContact))) {
+				throw new ElementoNoEncontradoException(lnkContact);
+			}
+
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			System.out.println(e.getMessage());
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+			System.out.println(e.getMessage());
+		} catch (ElementoNoEncontradoException e) {
+
+			System.out.println(e.getMessage());
 		}
 	}
 }
